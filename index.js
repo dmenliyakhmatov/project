cbaf5e071676283b82b5c3ce196af7e09ec32307
Dmitry Menliiakhmatov <menliiakhmatovdg@lad24.ru>
2021-05-14 20:14:44 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let a = 12;
+let b = 'sdfdsfds';
+let c = 25;
+let d = 16;
+function getValue() {
+  return a;
+}
+//sdsd commentsdsd
+function g23Value() {
+  return a;
+}
+function g2khjgValue() {
+  return a;
+}
+function g2khjdffValue() {
+  return a;
+}
+
+function setValue() {
+  return a;
+}
+function setrValue() {
+  return a;
+}
